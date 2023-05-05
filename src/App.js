d21240dc283c4267f97a25f47313468fbb965a1f
NicolasDevJS <nicolas.cmelo@outlook.com>
2023-05-04 23:09:56 -0300

    css

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+//Importações dos componentes
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/modelo.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+
+//importações
+import Logo from "../assets/img/logo.svg";
+import Nav from "../components/Nav";
+import NavMobile from "./NavMobile";
+import Socials from "./Socials";
+
+function Header() {
+  return (
+    <header>
+      <h1 class="text-30x30 font-bold underline text-start ">Hello wo!</h1>
+    </header>
+  );
+}
+
+export default Header;
